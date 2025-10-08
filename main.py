cebdbd1f6709aed2208eb576f77a53b6e30e2863
Jhonatan-Dias <jhonatanpublicol7@gmail.com>
2025-10-08 11:37:34 -0300

    Criação da minha estrutura do briefin 3 sobre a clinica veterinaria, criação do app e dentro do app tem as pastas controller, database, models, routes e views. Cada pasta tem uma função para o codigo para no fim, todas serem conectadas ao main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from app.routes import rota_usuario, rota_produto, rota_movimentacao
 
 if __name__ == '__main__':
     app.run(debug=True)
+Criação da minha estrutura do briefin 3 sobre a clinica veterinaria, criação do app e dentro do app tem as pastas controller, database, models, routes e views. Cada pasta tem uma função para o codigo para no fim, todas serem conectadas ao main.py
