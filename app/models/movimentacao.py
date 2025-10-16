aa3643fe6e7cdc0543841131b4befb74f881bedb
femanuelbezerraa <f.alencar@aluno.senai.br>
2025-10-16 11:23:35 -0300

    Colocação do script do banco de dados, ajustes no css de cadastro deproduto e ajuste emovimentacai.py para adicionar o id do usuario na tabela em estoque.

--- app/models/movimentacao.py
+++ app/models/movimentacao.py
@@ -18,7 +18,7 @@ class Movimentacao:
     @staticmethod
     def listar_todas():
         return query_db('''
-            SELECT m.id_movimentacao AS id, m.produto_id, p.nome AS nome_produto, m.tipo_movimentacao, m.quantidade, m.data_movimentacao, u.nome AS usuario_nome
+            SELECT m.id_movimentacao AS id, m.produto_id, p.nome AS nome_produto, m.tipo_movimentacao, m.quantidade, m.data_movimentacao, u.nome AS usuario_nome, m.usuario_id AS usuario_id
             FROM movimentacao AS m
             JOIN produto AS p ON m.produto_id = p.id_produto
             JOIN usuario AS u ON m.usuario_id = u.id_usuario
